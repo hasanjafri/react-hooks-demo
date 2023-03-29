6a2ee28dc89bd432d7c68896a58a2f583b570966
Hasan <syed_jafri@manulife.ca>
2023-03-29 13:19:59 -0400

    initialized demo repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import useLocalStorage from './hooks/useLocalStorage';
+
 function App() {
+  const [count, setCount] = useLocalStorage('count', 0);
+  // const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +16,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Count: {count}
+        </p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
         <a
           className="App-link"
           href="https://reactjs.org"
